d4a8d9ef331226a41064da377f141978b63642c9
Mauro Pisu <pisu.maru@gmail.com>
2023-03-11 11:31:32 +0100

    delete Event

--- app/src/main/java/com/example/myapplication/EventsInDay.kt
+++ app/src/main/java/com/example/myapplication/EventsInDay.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.android.volley.AuthFailureError
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -16,12 +18,23 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.HashMap
 
-var localDateTime: LocalDate = LocalDate.now()
-var items = mutableListOf<String>()
-var GUser : String = ""
+class Events{
+    fun clear(){
+        descriptions.clear()
+        IDs.clear()
+        tables.clear()
+    }
+
+    var descriptions = arrayListOf<String>()
+    val tables = arrayListOf<String>()
+    val IDs = arrayListOf<Int>()
+}
 
 class EventsInDay : AppCompatActivity() {
 
+    var localDateTime: LocalDate = LocalDate.now()
+    private var items = Events()
+    private var gUser : String = ""
     private lateinit var binding : ActivityEventsInDayBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +45,12 @@ class EventsInDay : AppCompatActivity() {
 
         val myIntent = intent // gets the previously created intent
 
-        GUser = myIntent.getStringExtra("user_id").toString()
+        gUser = myIntent.getStringExtra("user_id").toString()
         val year: Int = myIntent.getIntExtra("year", today.year)
         val month: Int = myIntent.getIntExtra("month", today.monthValue)
         val day: Int = myIntent.getIntExtra("day", today.dayOfMonth)
+
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this)
 
         localDateTime = LocalDate.of(year, month, day)
 
@@ -44,9 +59,26 @@ class EventsInDay : AppCompatActivity() {
 
         fillList()
 
+
+        binding.eventDayList.setOnItemClickListener{ parent, _, position, _ ->
+            alertDialog.setTitle(getString(R.string.delete_entry))
+            alertDialog.setMessage(getString(R.string.delete_entry_confirmation))
+            alertDialog.setPositiveButton(
+                "yes"
+            ) { _, _ ->
+                deleteElement(items.tables[position], items.IDs[position])
+            }
+            alertDialog.setNegativeButton(
+                "No"
+            ) { _, _ ->}
+            val alert: AlertDialog = alertDialog.create()
+            alert.setCanceledOnTouchOutside(false)
+            alert.show()
+        }
+
         binding.floatingAddSymptom.setOnClickListener(){
             val intent = Intent(this@EventsInDay, InsertSymptom::class.java).apply {}
-            intent.putExtra("user_id",GUser)
+            intent.putExtra("user_id",gUser)
             intent.putExtra("year",year)
             intent.putExtra("month",month)
             intent.putExtra("day",day)
@@ -55,7 +87,7 @@ class EventsInDay : AppCompatActivity() {
 
         binding.floatingAddMeal.setOnClickListener(){
             val intent = Intent(this@EventsInDay, InsertMeal::class.java).apply {}
-            intent.putExtra("user_id",GUser)
+            intent.putExtra("user_id",gUser)
             intent.putExtra("year",year)
             intent.putExtra("month",month)
             intent.putExtra("day",day)
@@ -64,7 +96,7 @@ class EventsInDay : AppCompatActivity() {
 
         binding.floatingAddFeces.setOnClickListener(){
             val intent = Intent(this@EventsInDay, InsertFeces::class.java).apply {}
-            intent.putExtra("user_id",GUser)
+            intent.putExtra("user_id",gUser)
             intent.putExtra("year",year)
             intent.putExtra("month",month)
             intent.putExtra("day",day)
@@ -73,7 +105,7 @@ class EventsInDay : AppCompatActivity() {
 
         binding.floatingAddSupplement.setOnClickListener(){
             val intent = Intent(this@EventsInDay, InsertSupplement::class.java).apply {}
-            intent.putExtra("user_id",GUser)
+            intent.putExtra("user_id",gUser)
             intent.putExtra("year",year)
             intent.putExtra("month",month)
             intent.putExtra("day",day)
@@ -115,15 +147,44 @@ class EventsInDay : AppCompatActivity() {
         val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
         val data = jsonObject.getJSONArray("data")
         (0 until data.length()).forEach {
-            val book = data.getJSONObject(it)
-            val value = book.get("value").toString()
-            val date = LocalDate.parse(book.get("date").toString(), dateFormatter)
-            val time = LocalTime.parse(book.get("time").toString(), timeFormatter)
+            val event = data.getJSONObject(it)
+            val value = event.get("value").toString()
+            val table = event.get("table").toString()
+            val id = event.get("id").toString().toInt()
+            val date = LocalDate.parse(event.get("date").toString(), dateFormatter)
+            val time = LocalTime.parse(event.get("time").toString(), timeFormatter)
             if (date == localDateTime){
-                items.add("$time - $value")
+                items.descriptions.add("$time - $value")
+                items.IDs.add(id)
+                items.tables.add(table)
             }
         }
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+            items.descriptions)
         binding.eventDayList.adapter = adapter
+    }
+
+    private fun deleteElement(table: String, itemId: Int){
+        val url = Utils.composeUrl(
+            GUserId, "table/$table/$itemId"
+        )
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectRequest: JsonObjectRequest = object : JsonObjectRequest(
+            Method.DELETE, url, null,
+            Response.Listener {
+                Log.d("Mainactivity", getString(R.string.api_call_successful))
+            }, Response.ErrorListener {
+                Log.d("Mainactivity", getString(R.string.api_call_unsuccessful)+it.toString())
+            }
+        ){
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> = HashMap()
+                headers["X-Session-Token"] = "abcd"
+                return headers
+            }
+        }
+        queue.add(jsonObjectRequest)
+
     }
 }
